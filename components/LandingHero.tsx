d704dd83e01cb7f873ae33d210de66450a216eb0
thieutrung2590 <thieutrung2590@gmail.com>
2026-03-29 01:25:23 +0700

    Update LandingHero.tsx

--- components/LandingHero.tsx
+++ components/LandingHero.tsx
@@ -93,7 +93,7 @@ export default function LandingHero({ siteName }: LandingHeroProps) {
           >
             <span className="relative z-10 flex items-center gap-3">
               <BookOpen className="size-5 text-amber-700 group-hover:scale-110 transition-transform" />
-              Giới thiệu dòng họ
+              Giới thiệu dòng họ Nguyễn Thiệu
             </span>
           </Link>
 
